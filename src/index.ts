d168ba1eebb5213f486861912b66cdf350ce04ae
Patric <dean@schoenenberger-software.de>
2021-10-31 03:40:37 +0100

    created lorem ipsum api call

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-/**
- * 
- * @param a number
- * @param b number
- * @returns Number
- */
-export function add(a: number, b: number) {
-    return a + b;
+const configuration: object = {
+    "type": "meat-and-filler", "start-with-lorem": 1
+}
+
+export async function loremIpsum(config: object = configuration) {
+    let response = await fetch("https://baconipsum.com/api/", {
+        "body": JSON.stringify(config)
+    })
+
+    let data = await response.json()
+    return data;
 }
