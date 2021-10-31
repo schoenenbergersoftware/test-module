faea6a5f9801fc9793fce85bf785f9fe77aac63a
Patric <dean@schoenenberger-software.de>
2021-10-31 02:25:12 +0100

    First Test

---
+++ lib/index.d.ts
@@ -0,0 +1,8 @@
+/**
+ *
+ * @param a number
+ * @param b number
+ * @returns Number
+ */
+export declare function add(a: number, b: number): number;
+//# sourceMappingURL=index.d.ts.map
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * 
+ * @param a number
+ * @param b number
+ * @returns Number
+ */
+export function add(a: number, b: number) {
+    return a + b;
+}
